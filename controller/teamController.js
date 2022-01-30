6308170917cc6b83e4b6457833721762153ff740
Yossawadee Hoymala <h.yossawadee@gmail.com>
2022-01-30 21:21:05 +0700

    teams check not found

--- controller/teamController.js
+++ controller/teamController.js
@@ -1,19 +1,41 @@
-var UserTeamRlt = require("../model/teamModel")
+var UserTeamRlt = require("../model/teamModel");
 
-exports.getAllTeams = function(req, res){
-    UserTeamRlt.getAllTeams(req.params.user_id, function(err, teams){
-        if(err){
-            res.send(err)
-        }
-        res.json(teams)
-    })
-}
+exports.getAllTeams = function (req, res) {
+  UserTeamRlt.getAllTeams(req.params.user_id, function (err, rlt) {
+    if (err) {
+      return res.status(500).json({
+        status: 500,
+        message: "Internal Server Error",
+      });
+    }
 
-exports.getTeamById = function(req, res){
-    UserTeamRlt.getTeamById(req.params.user_id, req.params.team_id, function(err, teams){
-        if(err){
-            res.send(err)
-        }
-        res.json(teams)
-    })
-}
+    if (rlt.length === 0)
+      return res.status(400).json({
+        status: 400,
+        message: `No teams of user_id = ${req.params.user_id} or not found`,
+      });
+    res.json(rlt);
+  });
+};
+
+exports.getTeamById = function (req, res) {
+  UserTeamRlt.getTeamById(
+    req.params.user_id,
+    req.params.team_id,
+    function (err, rlt) {
+      if (err) {
+        return res.status(500).json({
+          status: 500,
+          message: "Internal Server Error",
+        });
+      }
+
+      if (rlt.length === 0)
+        return res.status(400).json({
+          status: 400,
+          message: `Not found`,
+        });
+      res.json(rlt);
+    }
+  );
+};
